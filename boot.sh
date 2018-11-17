b9cc200f805342cbe5468dc473500fcdd868375e
Martin Winter <git@macwinnie.me>
2018-11-17 20:26:35 +0100

    additionally copy environmental file from example

--- boot.sh
+++ boot.sh
@@ -3,5 +3,8 @@
 ###
 
 cd $APACHE_WORKDIR
+if [ ! -f .env ]; then
+    cp .env.example .env
+fi
 sudo -u $WORKINGUSER composer global require hirak/prestissimo
 sudo -u $WORKINGUSER composer install --prefer-dist
